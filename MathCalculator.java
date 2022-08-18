c19c42d0c1ea9ba97a8955c754f451d5245285e7
sairaj <pawarsairaj6@gmail.com>
2022-08-18 22:45:59 +0530

    calculator

---
+++ MathCalculator.java
@@ -0,0 +1,30 @@
+public class MathCalculator {
+
+	public static void main(String[] args) {
+		
+		MathCalculator math = new MathCalculator();
+		
+		math.Addition();
+		math.Sbutraction();
+		math.Mutiplication();
+		math.Division();
+	}
+	
+	public void Addition()
+	{
+		System.out.println("im Addition");
+	}
+	public void Sbutraction()
+	{
+		System.out.println("im Sbutraction");
+	}
+	public void Mutiplication()
+	{
+		System.out.println("im Mutiplication");
+	}
+	public void Division()
+	{
+		System.out.println("im Division");
+	}
+
+}
